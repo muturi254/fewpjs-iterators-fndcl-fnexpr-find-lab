04a41ba8ff7aff2fd41b9b5715fdc26a5dbc9b68
muturi25 <petermuturi74@gmail.com>
2022-01-24 17:01:57 +0300

    temp solution

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(dataSet) {
+    let obj = dataSet.find(data => data.result === "W");
+    
+    if (obj) {
+        return obj.year
+    }
+    return undefined;
+}
 
+// let dataSet = [
+//     { year: "2018", result: "Lf" },
+//     { year: "2017", result: "W" },
+//     { year: "2016", result: "N/A" }
+// ];
+
+// let john = superbowlWin(dataSet);
+// console.log(john);
